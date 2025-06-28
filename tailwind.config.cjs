25a68b05a267c228eff80f668d4d5351298a3a05
maaria0 <191508154+maaria0@users.noreply.github.com>
2025-06-28 13:45:51 +0530

    Update login page UI using Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This is important to scan all files using Tailwind
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // optional custom colors
+      },
+    },
+  },
+  plugins: [],
+};
+
+
